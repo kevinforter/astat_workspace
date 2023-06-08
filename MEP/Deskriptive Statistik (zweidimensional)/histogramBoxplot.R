26aed1d4eec95812ffbebb2b2079d7c393a0d7ad
kevinforter <49223606+kevinforter@users.noreply.github.com>
2023-06-08 17:53:11 +0200

    commit changes

---
+++ MEP/Deskriptive Statistik (zweidimensional)/histogramBoxplot.R
@@ -0,0 +1,29 @@
+geysir <- read.table("SW03/Unterricht/geysir.dat")
+par(mfrow = c(2, 2))
+head(geysir)
+hist(geysir [, "Zeitspanne"],
+     main = "Zeitspanne",
+     col = "darkseagreen3",
+     xlab = "Zeitspanne zwischen zwei Ausbrüchen (in Min.)",
+     ylab = "Häufigkeit",
+)
+
+hist(geysir [, "Eruptionsdauer"],
+     main = "Eruptionsdauer",
+     breaks =  14,
+     col = "darkseagreen3",
+     xlab = "Zeitspanne eines Ausbruchs (in Min.)",
+     ylab = "Häufigkeit",
+)
+
+boxplot(geysir [, "Zeitspanne"],
+        col = "darkseagreen",
+        xlab = "Zeitspanne zwischen zwei Ausbrüchen (in Min.)",
+        horizontal = TRUE
+)
+
+boxplot(geysir [, "Eruptionsdauer"],
+        col = "darkseagreen",
+        xlab = "Zeitspanne eines Ausbruchs (in Min.)",
+        horizontal = TRUE
+)
--- SW03/Unterricht/histogramGeysir.R
+++ SW03/Unterricht/histogramGeysir.R
@@ -2,12 +2,14 @@ geysir <- read.table("SW03/Unterricht/geysir.dat")
 par(mfrow = c(2, 2))
 head(geysir)
 hist(geysir [, "Zeitspanne"],
+        main = "Zeitspanne",
         col = "darkseagreen3",
         xlab = "Zeitspanne zwischen zwei Ausbrüchen (in Min.)",
         ylab = "Häufigkeit",
 )
 
 hist(geysir [, "Eruptionsdauer"],
+        main = "Eruptionsdauer",
         breaks =  14,
         col = "darkseagreen3",
         xlab = "Zeitspanne eines Ausbruchs (in Min.)",
